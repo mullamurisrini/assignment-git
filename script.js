f2f1a57c6d3ec01139dfdaa5414e989fde152ebf
Srinitha Mullamuri <mullamurisrinitha@gmail.com>
2025-08-20 16:38:22 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,5 @@
+const name = "Your Srini";  // Replace with your real name
+const currentDate = new Date().toLocaleString();
+
+console.log(`Name: ${name}`);
+console.log(`Current Date: ${currentDate}`);
